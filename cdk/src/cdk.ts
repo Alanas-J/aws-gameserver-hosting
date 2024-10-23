981b76cbabc8b9324cbb81f08974b0ea5e652171
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-10-23 19:48:44 +0100

    AWSGS-5: VPC/subnet config

--- cdk/src/cdk.ts
+++ cdk/src/cdk.ts
@@ -2,10 +2,23 @@
 import { CfnOutput } from "aws-cdk-lib"
 import { Stack } from "aws-cdk-lib"
 import { App } from "aws-cdk-lib"
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2"
 
 const app = new App()
 export const gameServerStack = new Stack(app, 'GameServerStack', {
     description: 'Gameserver hosting on AWS.'
+})
+
+const vpc = new Vpc(gameServerStack, 'VPC', {
+    vpcName: 'GameServerVPC',
+    cidr: '10.0.255.0/24', // Arbitrary choice, can be expanded for space for a fleet of instances.
+    subnetConfiguration: [
+        {
+            name: 'GameServerPublicSubnet',
+            cidrMask: 28, // 16 hosts
+            subnetType: SubnetType.PUBLIC,
+        }
+    ]
 })
 
 new CfnOutput(gameServerStack, 'HelloWorld', { value: 'Hello world, a pointless cloudformation output!'})
