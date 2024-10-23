acbcb73800ba41b41074a05cdb763c849c1028ee
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-10-23 21:27:38 +0100

    AWSGS-5: VPC/subnet config (#1)
    * AWSGS-5: VPC/subnet config
    
    * AWSGS-5: Reduced availability zones to 1, strictly set NAT gates to 0,
    
    + updated cidr config to the new syntax

--- cdk/src/cdk.ts
+++ cdk/src/cdk.ts
@@ -2,10 +2,25 @@
 import { CfnOutput } from "aws-cdk-lib"
 import { Stack } from "aws-cdk-lib"
 import { App } from "aws-cdk-lib"
+import { IpAddresses, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2"
 
 const app = new App()
 export const gameServerStack = new Stack(app, 'GameServerStack', {
     description: 'Gameserver hosting on AWS.'
+})
+
+const vpc = new Vpc(gameServerStack, 'VPC', {
+    vpcName: 'GameServerVPC',
+    ipAddresses: IpAddresses.cidr('10.0.255.0/24'), // Arbitrary choice, can be expanded for space for a fleet of instances.
+    subnetConfiguration: [
+        {
+            name: 'GameServerPublicSubnet',
+            cidrMask: 28, // 16 hosts
+            subnetType: SubnetType.PUBLIC,
+        }
+    ],
+    maxAzs: 1,
+    natGateways: 0
 })
 
 new CfnOutput(gameServerStack, 'HelloWorld', { value: 'Hello world, a pointless cloudformation output!'})
