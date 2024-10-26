f92c9cd53fb64b8fbfbf7e9ca1bdf9baf1882ff0
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-10-26 23:48:11 +0100

    AWSGS-9: start of EC2 provisioning; securitygroup config

---
+++ cdk/src/constructs/ec2-provisioning-construct/ec2-provisioning-construct.ts
@@ -0,0 +1,41 @@
+import { Stack } from "aws-cdk-lib";
+import { Instance, Peer, Port, SecurityGroup, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+
+
+export class EC2ProvisioningConstruct extends Construct {
+
+    constructor(parent: Construct, vpc: Vpc, servermasterLambdaSG: SecurityGroup) {
+        super(parent, 'EC2Provision');
+
+        const gameserverSecurityGroup = new SecurityGroup(this, 'SecurityGroup', {
+            vpc,
+            allowAllOutbound: true, // Need to allow for installation.
+            description: 'Gameserver Security Group',
+        });
+
+        // Allowing SSH only from EC2 Instance Connect via the AWS Console.
+        gameserverSecurityGroup.addIngressRule(Peer.prefixList(`com.amazonaws.${Stack.of(this).region}.ec2-instance-connect`), Port.tcp(22), 'Allow SSH access via EC2 Instance Connect.');
+
+        // Lambda @TODO: may need to change, might not use port 80 for node, yet to see. (might not want to give Node privelege enough to use port 80, but no idea how much power the 'ec2-user' has.)
+        gameserverSecurityGroup.addIngressRule(Peer.securityGroupId(servermasterLambdaSG.securityGroupId), Port.tcp(80), 'Allow the servermaster lambda to access the HTTP port.');
+
+        // Minecraft Port
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(25565), 'Allow all ipv4/tcp to connect to Minecraft.');
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(25565), 'Allow all ipv4/udp to connect to Minecraft.');
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv6(), Port.tcp(25565), 'Allow all ipv6/tcp to connect to Minecraft.');
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv6(), Port.udp(25565), 'Allow all ipv6/udp to connect to Minecraft.');
+
+        // Factorio Port
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.udp(34197), 'Allow all ipv4/udp to connect to Factorio.');
+        gameserverSecurityGroup.addIngressRule(Peer.anyIpv6(), Port.udp(34197), 'Allow all ipv6/udp to connect to Factorio.');
+
+
+        ['1'].map(instanceConfig => {
+
+            return new Instance(this, 'Instance')
+
+        }) 
+
+    }
+}
