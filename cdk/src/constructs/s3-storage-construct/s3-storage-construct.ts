f898d83a3dc0914b5896593e92beeface3944747
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-10-26 20:43:35 +0100

    AWSGS-7: changed bucket name to be generated by the CDK
    There's no way to re-attach a retained bucket to the stack.
    The best option is to allow the CDK to just create a new different named bucket
    
    and let the user remediate importing data from the old bucket

--- cdk/src/constructs/s3-storage-construct/s3-storage-construct.ts
+++ cdk/src/constructs/s3-storage-construct/s3-storage-construct.ts
@@ -12,13 +12,13 @@ export class S3StorageConstruct extends Construct {
         super(parent, 'S3Storage')
 
         this.s3Bucket = new Bucket(this, 'Bucket', {
-            bucketName: `gameserver-s3-storage-bucket`,
             publicReadAccess: false,
             removalPolicy: config.S3_BUCKET_REMOVAL_POLICY,
             autoDeleteObjects: config.S3_BUCKET_REMOVAL_POLICY === RemovalPolicy.DESTROY,
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
             accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL
-        })
+        });
+        
         new BucketDeployment(this, 'BucketDeploy', {
             sources: [Source.asset('../ec2_code')], // @TODO: Configure path to build output.
             destinationBucket: this.s3Bucket,
