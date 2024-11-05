7afc6023e34081173f46ccc2b94211d430d18799
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-11-05 20:06:43 +0000

    AWSGS-10: added assumed bash script layout

--- ec2_code/scripts/user_data.sh
+++ ec2_code/scripts/user_data.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo 'Installing Node...'
+echo '1: Installing Node...'
 sudo dnf install -y nodejs # May want to replace later with a specified Node.js version if bugs occur.
 
 
-echo 'Installing PM2...'
+echo '2: Installing PM2...'
 sudo npm install -g pm2
 
 
-echo 'Starting s3 sync...'
+echo '3: Starting s3 sync...'
 # @TODO: May want to detect the S3 bucket instead of hardcoding
 aws s3 sync s3://gameserverstack-s3storagebucketcf59ebf7-hidmmd95ycsc/ec2_code/ /opt/gameserver/
 chmod +x /opt/gameserver/*.sh
@@ -16,8 +16,10 @@ echo 'Sync complete!'
 
 
 # Not configured yet
-echo 'Adding startup script to crontab...'
-echo "@reboot /path/to/your/script.sh >> /path/to/logfile.log 2>&1" | crontab -
+echo '4: Adding startup script to crontab...'
+(crontab -l 2>/dev/null; echo "@reboot /opt/gameserver/scripts/boot_script.sh >> /var/log/boot_script.log 2>&1") | crontab -
 
+
+# Install CloudWatch Agent.
 
 # pm2 start will go here...
