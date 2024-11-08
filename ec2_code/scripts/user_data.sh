58cc06a033a19b09eaba2ca3db68d95fdeba0a7c
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-11-08 20:57:31 +0000

    AWSGS-23: userdata now creates a /var/gameserver directory which I'll use to store logs

--- ec2_code/scripts/user_data.sh
+++ ec2_code/scripts/user_data.sh
@@ -12,27 +12,34 @@ echo '2: Installing PM2... =====================================================
 npm install -g pm2@latest
 
 
-echo '3: Initial S3 directory setup... ===================================================================='
+echo '3: Directory Setup... ================================================================================'
+# Making ec2_user own all of this here for now
 mkdir -p /opt/gameserver
-chown ec2-user:ec2-user /opt/gameserver # Making ec2_user own all of this here for now
+chown ec2-user:ec2-user /opt/gameserver
+mkdir -p /opt/server_files
+chown ec2-user:ec2-user /opt/server_files
+mkdir -p /var/gameserver
+chown ec2-user:ec2-user /var/gameserver
+
+echo '4: Initial S3 sync... ==============================================================================='
 # @TODO: May want to detect the S3 bucket instead of hardcoding
 sudo -u ec2-user aws s3 sync s3://gameserverstack-s3storagebucketcf59ebf7-hidmmd95ycsc/ec2_code/ /opt/gameserver/
 shopt -s globstar # <---- allows for /**/ to be used in paths
 chmod +x /opt/gameserver/**/*.sh # Adding execution permissions for every shell file
 
 
-echo '4: Installing crontab and adding startup script to crontab... ========================================'
+echo '5: Installing crontab and adding startup script to crontab... ========================================'
 dnf install -y cronie
 systemctl start crond
 systemctl enable crond
-(sudo -u ec2-user crontab -l 2>/dev/null; echo "@reboot sudo sh -c '/opt/gameserver/scripts/boot_script.sh >> /var/log/boot_script.log 2>&1'") | sudo -u ec2-user crontab -
+(sudo -u ec2-user crontab -l 2>/dev/null; echo "@reboot sudo sh -c '/opt/gameserver/scripts/boot_script.sh >> /var/gameserver/boot_script.log 2>&1'") | sudo -u ec2-user crontab -
 
 
-echo '5: Installing and configuring Cloudwatch Agent (not implemented)...'
+echo '6: Installing and configuring Cloudwatch Agent (not implemented)...'
 # @TODO: Install CloudWatch Agent.
 
 
-echo '6: Installing, Building and Starting Node server... =================================================='
+echo '7: Installing, Building and Starting Node server... =================================================='
 sudo -u ec2-user npm --prefix /opt/gameserver install
 sudo -u ec2-user npm --prefix /opt/gameserver run build
 sudo -u ec2-user pm2 start /opt/gameserver/dist --name gameserver-node-app
