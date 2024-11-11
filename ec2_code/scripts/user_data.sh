f3acf45ff5804d53581044156db86c3ee0d59728
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-11-11 23:09:35 +0000

    AWSGS-23: working graceful shutdowns!
    Just need to add some delay for Cloudwatch to get shutdown logs
    
    + need to log code_sync script

--- ec2_code/scripts/user_data.sh
+++ ec2_code/scripts/user_data.sh
@@ -21,7 +21,19 @@ mkdir -p /var/gameserver/logs
 chown ec2-user:ec2-user /var/gameserver/logs
 
 
-echo '3: Server systemd configuration... ==================================================================='
+echo '3: Initial S3 pull... ================================================================================'
+# @TODO: May want to detect the S3 bucket instead of hardcoding
+sudo -u ec2-user aws s3 sync s3://gameserverstack-s3storagebucketcf59ebf7-hidmmd95ycsc/ec2_code/ /opt/gameserver/
+shopt -s globstar # <---- allows for /**/ to be used in paths
+chmod +x /opt/gameserver/**/*.sh # Adding execution permissions for every shell file
+
+
+echo '4: Installing and configuring Cloudwatch Agent... ===================================================='
+yum install -y amazon-cloudwatch-agent
+amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/gameserver/cloudwatch-config.json
+
+
+echo '5: Server systemd configuration... ==================================================================='
 SERVICE_NAME="gameserver-node-http-server"
 NODE_APP_DIR="/opt/gameserver"
 
@@ -32,13 +44,14 @@ Description=Gameserver's HTTP node app / gameserver controller.
 After=network.target
 
 [Service]
-ExecStartPre=$NODE_APP_DIR/scripts/utils/code-sync.sh
+ExecStartPre=$NODE_APP_DIR/scripts/utils/code_sync.sh
 ExecStart=/usr/bin/node $NODE_APP_DIR/dist/index.js
 WorkingDirectory=$NODE_APP_DIR
 Restart=always
+RestartSec=10 
 User=ec2-user
 Environment=PATH=/usr/bin:/usr/local/bin
-Environment=NODE_ENV=production
+TimeoutStopSec=60 
 
 [Install]
 WantedBy=multi-user.target
@@ -56,10 +69,6 @@ systemctl start "$SERVICE_NAME"
 echo "Checking status of $SERVICE_NAME..."
 systemctl status "$SERVICE_NAME" --no-pager
 
-
-echo '4: Installing and configuring Cloudwatch Agent... ===================================================='
-yum install -y amazon-cloudwatch-agent
-amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/gameserver/cloudwatch-config.json
 
 
 echo '====================================== EC2 INSTALL COMPLETE =========================================='
