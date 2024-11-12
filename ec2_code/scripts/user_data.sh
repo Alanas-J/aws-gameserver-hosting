717b4f983d377b5979d0e0998b81f32059d590f1
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2024-11-12 20:44:54 +0000

    AWSGS-24: initial userdata script tidyup, server now runs under a sandboxed user

--- ec2_code/scripts/user_data.sh
+++ ec2_code/scripts/user_data.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-# WARNING: changing this script and deploying terminates all provisioned instances.
+# WARNING: Changing this script and deploying terminates/recreates all provisioned instances.
+
+GAMESERVER_USER="gameserver-user"
+GAMESERVER_CODE_DIR="/opt/gameserver"
+GAMESERVER_SERVER_FILES_DIR="/opt/server_files"
+GAMESERVER_VAR_DIR="/var/gameserver"
+
 
 echo '0: Updating DNF... ==================================================================================='
-dnf update;
+dnf update
 
 
 echo '1: Installing Node... ================================================================================'
 dnf install -y nodejs # May want to replace later with a specified Node.js version if bugs occur.
 
 
-echo '2: Directory Setup... ================================================================================'
-# Making ec2_user own all of this here for now
-mkdir -p /opt/gameserver
-chown ec2-user:ec2-user /opt/gameserver
-mkdir -p /opt/server_files
-chown ec2-user:ec2-user /opt/server_files
-mkdir -p /var/gameserver
-chown ec2-user:ec2-user /var/gameserver
-mkdir -p /var/gameserver/logs
-chown ec2-user:ec2-user /var/gameserver/logs
+echo '2: Server User + Directory Setup... =================================================================='
+useradd -r -m -d "/home/$GAMESERVER_USER" -s /usr/sbin/nologin "$GAMESERVER_USER"
+mkdir -p $GAMESERVER_CODE_DIR
+chown -r $GAMESERVER_USER:$GAMESERVER_USER $GAMESERVER_CODE_DIR
+mkdir -p $GAMESERVER_SERVER_FILES_DIR
+chown -r $GAMESERVER_USER:$GAMESERVER_USER $GAMESERVER_SERVER_FILES_DIR
+mkdir -p $GAMESERVER_VAR_DIR/logs
+chown -r $GAMESERVER_USER:$GAMESERVER_USER $GAMESERVER_VAR_DIR
 
 
 echo '3: Initial S3 pull... ================================================================================'
 # @TODO: May want to detect the S3 bucket instead of hardcoding
-sudo -u ec2-user aws s3 sync s3://gameserverstack-s3storagebucketcf59ebf7-hidmmd95ycsc/ec2_code/ /opt/gameserver/
+sudo -u $GAMESERVER_USER aws s3 sync s3://gameserverstack-s3storagebucketcf59ebf7-hidmmd95ycsc/ec2_code/ $GAMESERVER_CODE_DIR/
 shopt -s globstar # <---- allows for /**/ to be used in paths
-chmod +x /opt/gameserver/**/*.sh # Adding execution permissions for every shell file
+chmod +x $GAMESERVER_CODE_DIR/**/*.sh # Adding execution permissions for every shell file
 
 
 echo '4: Installing and configuring Cloudwatch Agent... ===================================================='
 yum install -y amazon-cloudwatch-agent
-amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/gameserver/cloudwatch-config.json
+amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:$GAMESERVER_CODE_DIR/cloudwatch-config.json
 
 
 echo '5: Server systemd configuration... ==================================================================='
 SERVICE_NAME="gameserver-node-http-server"
-NODE_APP_DIR="/opt/gameserver"
 
 echo "Creating systemd service file for $SERVICE_NAME..."
 bash -c "cat <<EOL > /etc/systemd/system/$SERVICE_NAME.service
@@ -44,12 +47,12 @@ Description=Gameserver's HTTP node app / gameserver controller.
 After=network.target
 
 [Service]
-ExecStartPre=/bin/bash -c "$NODE_APP_DIR/scripts/utils/code_sync.sh >> '/var/gameserver/logs/code_sync.log' 2>&1"
-ExecStart=/usr/bin/node $NODE_APP_DIR/dist/index.js
-WorkingDirectory=$NODE_APP_DIR
+ExecStartPre=/bin/bash -c "$GAMESERVER_CODE_DIR/scripts/utils/code_sync.sh >> '$GAMESERVER_VAR_DIR/logs/code_sync.log' 2>&1"
+ExecStart=/usr/bin/node $GAMESERVER_CODE_DIR/dist/index.js
+WorkingDirectory=$GAMESERVER_CODE_DIR
 Restart=always
 RestartSec=10 
-User=ec2-user
+User=$GAMESERVER_USER
 Environment=PATH=/usr/bin:/usr/local/bin
 TimeoutStopSec=60 
 
